098321d0de40fbfdec9799c80e80fa6aa5e83606
Jared Napoli <janapoli@uci.edu>
2016-04-20 20:37:26 -0700

    actually added getGenreList(String query)

--- WEB-INF/sources/SQLClasses/Genre.java
+++ WEB-INF/sources/SQLClasses/Genre.java
@@ -21,7 +21,39 @@ public class Genre {
 		name = new_name;
 	}
 
+	public static List<Genre> getGenreList(String query)
+	{
+		Genre genre = new Genre();
+		List<Genre> genreList = new ArrayList<Genre>();
 
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Connection dbcon = DriverManager.getConnection("jdbc:mysql://localhost:3306/moviedb", "root", "root");
+			Statement statement = dbcon.createStatement();
+			//String query = "select * from movies where id in (select movie_id from stars_in_movies where star_id in (select id from stars where first_name= '" + first_name + "' and last_name= '" + last_name + "'))";
+
+			// Perform the query
+			ResultSet rs = statement.executeQuery(query);
+
+			// Iterate through each row of rs
+			while (rs.next()) {
+				movie = new Movie(
+				    rs.getInt("id"),
+				    rs.getString("name"),
+				genreList.add(genre);
+			}
+			rs.close();
+			statement.close();
+			dbcon.close();
+
+		} catch (Exception ex) {
+			while (ex != null) {
+				System.out.println ("SQL Exception 1:  " + ex.getMessage ());
+			}  // end while
+			return null;
+		}  // end catch SQLException
+		return genreList;
+	}
 
 	public static List<Genre> getFullGenreList() {
 		Genre genre = new Genre();
